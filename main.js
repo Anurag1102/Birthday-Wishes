d6e39a1192fbd3463405e0f51fd76189e1283cc9
AnuragDas <88575127+Anurag1102@users.noreply.github.com>
2021-12-21 20:38:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', fuction(Img)) }
+    block_image_object.scaleToWidth(700);                                                         
+    block_image_objectl.scaleToHeight(510);
+    top:"0";
+    left:"0";
+
+canvas.add(block_image_object);
+
+function playSound(){
+	x.play();
+}
